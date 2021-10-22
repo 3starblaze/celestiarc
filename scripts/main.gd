3ed3c2ae6085445a1eea7eff2ece5d04a941f612
3starblaze <3starblaze@protonmail.com>
2021-10-22 12:11:52 +0300

    Add a toggleable table in the main screen

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 const Meteor = preload("res://scenes/Meteor.tscn")
 const Helpers = preload("res://scripts/helpers.gd")
+var is_table_active = false
 onready var hud = $HUD
 onready var hp_label = $HUD/Panel/HpLabel
 onready var space_station = $SpaceStation
+onready var meteor_platform_table = $MeteorPlatformTable
 
 
 func _ready():
@@ -14,6 +16,13 @@ func _ready():
 	add_meteor(Vector2(100, 100), 300)
 	# warning-ignore:return_value_discarded
 	add_meteor(Vector2(100, 200), 200)
+
+
+func _process(_delta) -> void:
+	if Input.is_action_just_pressed("ui_up"):
+		show_table()
+	if Input.is_action_just_pressed("ui_down"):
+		hide_table()
 
 
 func _on_meteor_collision():
@@ -43,3 +52,17 @@ func px_to_canon_coord(coord: Vector2) -> Vector2:
 
 func canon_to_px_coord(coord: Vector2) -> Vector2:
 	return Helpers.canon_to_px_coord(coord, OS.window_size.y, hud.rect_size.y)
+
+
+func show_table() -> void:
+	if is_table_active:
+		return
+	meteor_platform_table.rect_position.y -= meteor_platform_table.rect_size.y
+	is_table_active = true
+
+
+func hide_table():
+	if not is_table_active:
+		return
+	meteor_platform_table.rect_position.y += meteor_platform_table.rect_size.y
+	is_table_active = false
